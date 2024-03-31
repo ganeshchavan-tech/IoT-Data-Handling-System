be72be0b72d88b011c42b7ff65aa274d5ecb94b3
Ganesh Chavan <chavanganesh229@gmail.com>
2024-03-31 12:17:07 +0530

    Code Optimization

--- publisher.py
+++ publisher.py
@@ -2,18 +2,18 @@ import pika
 from common.constants import *
 from services import connect_to_rabbitmq
 
-def publish_message(topic, payload):
+def publish_message(payload):
     try:
         channel = connect_to_rabbitmq()
 
         # Publish message
-        channel.basic_publish(exchange='', routing_key=topic, body=payload)
+        channel.basic_publish(exchange='', routing_key=QUEUE_NAME, body=payload)
 
-        print(f"Published message to topic '{topic}': {payload}")
+        print(f"Published message to topic '{QUEUE_NAME}': {payload}")
     
     except Exception as e:
         print(f"Failed to publish message: {e}")
 
 
 if __name__ == "__main__":
-    publish_message(QUEUE_NAME, '{"test_message2": "Hello, MQTT!"}')
+    publish_message('{"test_message2": "Hello, MQTT!"}')
